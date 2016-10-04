0590cae927ff0c29a551b77fe88100590f48de9c
Premysl Kafka <premysl.kafka@hpe.com>
2016-10-04 20:33:28 +0200

    Property IMAGE_ID removed

--- SimpleCrudServer/docker-build-and-push.sh
+++ SimpleCrudServer/docker-build-and-push.sh
@@ -2,10 +2,8 @@
 
 currentDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-IMAGE_NAME="kafkapre/linuxdays2016-simple-crud-server"
+IMAGE_NAME="kafkapre/linuxdays2016-simple-crud-server:latest"
 
-IMAGE_ID=`docker build -t $IMAGE_NAME  $currentDir/`
-
-docker tag $IMAGE_ID "$IMAGE_NAME:latest"
+docker build -t $IMAGE_NAME  $currentDir
 
 docker push $IMAGE_NAME
