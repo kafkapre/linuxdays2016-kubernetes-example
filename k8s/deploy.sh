c6a4e2db846fb2716b0d74b7386c37607693fa8e
Premysl Kafka <premysl.kafka@hpe.com>
2016-10-04 21:04:53 +0200

    Kubectl path is relative

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -4,14 +4,12 @@ currentDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source $currentDir/namespace.conf
 
-namespace=linuxdays
-
 echo "Creating namespace ..."
 sed -e "s/%namespaceName%/$namespace/" $currentDir/namespace.yaml | kubectl create -f -
 
 
 echo "Starting redis instance ..."
-/opt/kubectl --namespace=$namespace create -f $currentDir/redis.yaml
+kubectl --namespace=$namespace create -f $currentDir/redis.yaml
 
 echo "Starting SimpleCrudServer instance ..."
-/opt/kubectl --namespace=$namespace create -f $currentDir/SimpleCrudServer.yaml
+kubectl --namespace=$namespace create -f $currentDir/simpleCrudServer.yaml
--- k8s/undeploy.sh
+++ k8s/undeploy.sh
@@ -5,7 +5,7 @@ currentDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $currentDir/namespace.conf
 
 echo "deleting RCs and SVCs ..."
-kubectl --namespace=$namespace delete replicationcontrollers,pods,services --grace-period=0 -l "app=crud-server"
+kubectl --namespace=$namespace delete replicationcontrollers,pods,services -l "app=crud-server"
 
 echo "Deleting namespace ..."
 kubectl delete namespaces $namespace
